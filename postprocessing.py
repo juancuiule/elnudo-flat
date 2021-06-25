cef50b4d8211ec6af7ec85209734182b9c274a1a
Juan Ignacio Cuiule <juanchicuiu@gmail.com>
2021-06-25 12:59:08 -0300

    Update postprocessing.py

--- postprocessing.py
+++ postprocessing.py
@@ -12,9 +12,17 @@ if __name__ == "__main__":
         values = np.append(values, x["time"] / 1000)
     
     f = stats.gaussian_kde(values, 0.5)
+    
+    s = pd.Series(values)
+    ax = s.plot.kde()
+    fig = ax.get_figure()
+    fig.savefig('line plot.jpg', bbox_inches='tight', dpi=150)
 
     pairs = []
     for x in range(0, int(values.max())):
         pairs.append([x, f(x)[0], int(round(np.average(values))), len(values)])    
 
     pd.DataFrame(pairs).to_csv("change-blindness.csv", index=False, header=["time", "area", "average", "total"])
+    
+    fig.savefig('line plot.jpg', bbox_inches='tight', dpi=150)
+
