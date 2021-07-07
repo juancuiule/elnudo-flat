3a8473cf8052aed019c15832b3f333886630d632
Juan Ignacio Cuiule <juanchicuiu@gmail.com>
2021-07-07 03:39:04 -0300

    filter data, plot histogram

--- pp-libet.py
+++ pp-libet.py
@@ -1,7 +1,7 @@
 import pandas as pd
 # import numpy as np
 # from scipy import stats
-# import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 
 if __name__ == "__main__":
     json = pd.read_json('./libet-data.json')
@@ -9,5 +9,15 @@ if __name__ == "__main__":
     raw = []
     for x in json["data"]:
         if "pc" in x.keys() and "user" in x.keys() and "won" in x.keys():
-            raw.append([x["pc"], x["user"], x["won"]])
+            pc = x["pc"]
+            user = x["user"]
+            won = x["won"]
+            if pc <= 50 and user <= 50 and (won == "pc" or won == "user"):
+                raw.append([pc, user, won])
     pd.DataFrame(raw).to_csv("libet-raw-data.csv", index=True, header=["pc", "user", "won"])
+
+    df["user"].hist(grid=False, bins=50)
+    plt.xlabel('Puntos usuarie')
+    plt.ylabel('Número de partidas')
+    plt.title('Puntos de usuaries por partida')
+    plt.savefig("hist.png")
